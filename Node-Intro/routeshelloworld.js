1a83db4ca75ac668e226fce6faa9e3f98d7f70ce
Jason-Maingot <114204036+Jason-Maingot@users.noreply.github.com>
2023-09-20 18:25:34 -0400

    Lab 3
    Followed slides for lab 3, lab 2 was complete with initial commit, but was updated here

--- Node-Intro/routeshelloworld.js
+++ Node-Intro/routeshelloworld.js
@@ -1,25 +1,22 @@
 // Setup our server
-const http = require('http');
-
+const http = require('http')
 const port = process.env.PORT || 3000;
-
 const server = http.createServer((request, response) => {
-// Get the url
-const path = request.url;
+    const path = request.url
+    if ((request.url == "") || (request.url == "/")) {
+        response.writeHead(200 , { "Content-Type" : "Text/Plain" });
+        response.end("Home Page");
+    } else if (request.url == "/contact") {
+        response.writeHead(200 , { "Content-Type" : "text/plain" })
+        response.end("Contact Us")
+    } else if (request.url == "/about") {
+        response.writeHead(200 , { "Content-Type" : "text/plain" })
+        response.end("About Us")
+    } else {
+        response.writeHead(404 , { "Content-Type" : "text/plain"})
+        response.end("Not Found")
+    }
 
-if ((path == "") || (path == "/")) {
-    response.writeHead(200 , { "Content-Type" : "Text/Plain" });
-    response.end("Home Page");
-} else if (path == "/about") {
-    response.writeHead(200 , { "Content-Type" : "Text/Plain" });
-    response.end("About Page");
-} else if (path == "/contact") {
-    response.writeHead(200 , { "Content-Type" : "Text/html" });
-    response.end("<h1>Contact Us</h1>");
-} else {
-    response.writeHead(404 , { "Content-Type" : "Text/Plain" });
-    response.end("Not Found");
-}
 })
 
 // Start Server
